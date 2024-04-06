5e3eae9cf2d937ea448829c310ab7f740914fd1e
eike <98343869+EikeMB@users.noreply.github.com>
2024-04-06 18:22:29 -0400

    ui for login page done
    next step: register page ui
    implement firebase login using email and password

--- hour_log/lib/main.dart
+++ hour_log/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:hour_log/screens/authentication/login.dart';
 
 
 
@@ -9,7 +10,9 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp());
+  runApp(const MaterialApp(
+    home: SignIn(),
+  ));
 }
 
 
---
+++ hour_log/lib/screens/authentication/login.dart
@@ -0,0 +1,98 @@
+
+
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget{
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+final _formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+  String error = '';
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        actions: [
+          TextButton.icon(
+            style: ButtonStyle(
+              iconColor: MaterialStateProperty.all(Colors.black)
+            ),
+            onPressed: () {
+            },
+            icon: const Icon(Icons.person),
+            label: const Text(
+              'Register',
+              style: TextStyle(
+                color: Colors.black
+              )
+            ),
+          )
+        ],
+        backgroundColor: Colors.purple[200],
+        elevation: 0.0,
+        title: const Text('Sign in'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              TextFormField(
+                validator:(value) => value!.isEmpty ? 'Enter an email' : null,
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20.0,),
+              TextFormField(
+                validator: (value) => value!.length < 6 ? 'Enter a password 6+ characters long' : null,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 20.0,),
+              ElevatedButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.purple[300])),
+                onPressed: () {
+                  setState(() {
+                    error = 'Sign in unsuccessful';
+                  });
+                },
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 16.0,),
+              Text(
+                error,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
